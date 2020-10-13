47a3fd73c6a780a5ffb06408fb31b17087ee5c19
Ivan Stadnik <stadnikxivan@gmail.com>
2020-10-13 21:29:03 +0200

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let strNumber = Math.abs(n) + '';
+    let result = '';
+    for (i = 0; i < strNumber.length; i++) {
+      result = strNumber.substr(i, 1) + result;
+    };
+    return result*1;
 }
